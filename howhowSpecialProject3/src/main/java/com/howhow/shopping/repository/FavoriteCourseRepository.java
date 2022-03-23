05672690c89921dc0f3f939993b1341290e5066b
LiChien821 <95334484+LiChien821@users.noreply.github.com>
2022-03-23 12:26:28 +0800

    Delete howhowSpecialProject3 directory

--- howhowSpecialProject3/src/main/java/com/howhow/shopping/repository/FavoriteCourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.howhow.shopping.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.howhow.entity.FavoriteCourse;
-
-public interface FavoriteCourseRepository extends JpaRepository<FavoriteCourse, Integer> {
-
-}
--- howhowSpecialProject3/src/main/java/com/howhow/shopping/repository/OrderMtRepository.java
+++
@@ -1,9 +0,0 @@
-package com.howhow.shopping.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.howhow.entity.OrderMt;
-
-public interface OrderMtRepository extends JpaRepository<OrderMt, Integer> {
-
-}
