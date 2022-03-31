b8ef867aba978ad1592c8c6d5ae864647a02189a
DESKTOP-AARVA5E\91 <kyoko7879@gmail.com>
2022-03-31 19:59:56 +0800

    #google抓取資料

--- howhowSpecialProject/src/main/java/com/howhow/account/controller/AccountController.java
+++ howhowSpecialProject/src/main/java/com/howhow/account/controller/AccountController.java
@@ -7,6 +7,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,74 +21,75 @@ import com.howhow.entity.UserAccountDt;
 import com.howhow.entity.UserAccountMt;
 import com.howhow.websecurity.AccountUserDetails;
 
-
 @Controller
 public class AccountController {
-	
+
 	@Autowired
 	private AccountService service;
-	
+
 	@GetMapping("/index")
 	public String processTest() {
 		return "index";
 	}
-	
+
+
 	@GetMapping("/login")
 	public String login() {
-		Authentication authentication =SecurityContextHolder.getContext().getAuthentication();
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
 			return "login";
 		}
 		return "redirect:/home";
 	}
-	
+
 	@GetMapping("/home")
-	public String index(@AuthenticationPrincipal AccountUserDetails loggedAccount ,OAuth2AuthenticationToken googlelogged ,Model model) {
-		
-		if(loggedAccount==null || googlelogged==null) return "redirect:/";
-		
-		UserAccountMt account=loggedAccount.getLoggedAccount();
-		model.addAttribute("account",account);
+	public String index(@AuthenticationPrincipal AccountUserDetails loggedAccount,
+			OAuth2AuthenticationToken googlelogged, Model model) {
+
+		if (loggedAccount == null || googlelogged == null)
+			return "redirect:/";
+
+		UserAccountMt account = loggedAccount.getLoggedAccount();
+		model.addAttribute("account", account);
 		System.out.println(account.getUserId());
-	 
+
 		return "main";
 	}
-	
-	
+
 	@GetMapping("/editAccount")
-	public String editPage(@AuthenticationPrincipal AccountUserDetails loggedAccount ,Model model) {
-		
-		UserAccountMt loggedaccount=loggedAccount.getLoggedAccount();
-		UserAccountMt account=service.findByUserAccount(loggedaccount.getAccount());
-		UserAccountDt accountDetail =account.getUserAccountDt();
-		model.addAttribute("account",account);
-		model.addAttribute("accountDetail",accountDetail);
+	public String editPage(@AuthenticationPrincipal AccountUserDetails loggedAccount, Model model) {
+
+		UserAccountMt loggedaccount = loggedAccount.getLoggedAccount();
+		UserAccountMt account = service.findByUserAccount(loggedaccount.getAccount());
+		UserAccountDt accountDetail = account.getUserAccountDt();
+		model.addAttribute("account", account);
+		model.addAttribute("accountDetail", accountDetail);
 		return "editaccount";
 	}
-	
+
 	@GetMapping("/progessDeleteAccount")
-	public String processDelete(@AuthenticationPrincipal AccountUserDetails loggedAccount ,Model model) {
-		
-		UserAccountMt account=loggedAccount.getLoggedAccount();
-		
-		if(("admin").equals(account.getUserstatus().getAccountLevel().toString())) {
+	public String processDelete(@AuthenticationPrincipal AccountUserDetails loggedAccount, Model model) {
+
+		UserAccountMt account = loggedAccount.getLoggedAccount();
+
+		if (("admin").equals(account.getUserstatus().getAccountLevel().toString())) {
 			service.deleteAccount(account);
 			return "redirect:/logout";
-			
+
 		}
-		
+
 		return "redirect:/";
-	
-		
+
 	}
-	
+
 	@PostMapping("/progessEditAccount")
-	public String processEdit(@ModelAttribute("account")UserAccountMt account ,@ModelAttribute("accountDetail") UserAccountDt acd,Model model) {
+	public String processEdit(@ModelAttribute("account") UserAccountMt account,
+			@ModelAttribute("accountDetail") UserAccountDt acd, Model model) {
 		account.setUserAccountDt(acd);
 		service.edit(account);
-		
-		model.addAttribute("text","修改已完成");
+
+		model.addAttribute("text", "修改已完成");
 		return "success";
 	}
-	
+
 }
