be2d5d43f2faa4365d7ad6cd45823105cd0b7f69
DESKTOP-AARVA5E\91 <kyoko7879@gmail.com>
2022-04-04 14:34:09 +0800

    #editpassword

--- howhowSpecialProject/src/main/java/com/howhow/account/controller/AccountController.java
+++ howhowSpecialProject/src/main/java/com/howhow/account/controller/AccountController.java
@@ -1,23 +1,18 @@
 package com.howhow.account.controller;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.config.web.servlet.oauth2.login.OAuth2LoginSecurityMarker;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.howhow.account.service.AccountService;
 import com.howhow.entity.UserAccountDt;
@@ -30,6 +25,9 @@ public class AccountController {
 
 	@Autowired
 	private AccountService service;
+	
+	@Autowired
+	private PasswordEncoder bcryptoEncoder;
 
 	@GetMapping("/index")
 	public String processTest() {
@@ -47,39 +45,49 @@ public class AccountController {
 	}
 
 	@GetMapping("/home")
-	public String home( Model model) {
-		 Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		  String email="";
-		  if (principal instanceof AccountUserDetails) {
-		   email = ((AccountUserDetails)principal).getEmail();
-		  } else if(principal instanceof  DefaultOidcUser){
-		    email = ((DefaultOidcUser)principal).getEmail();
-		  }else {
-		   return "redirect:/";
-		  }
-		  UserAccountDt udt=  service.findByEmail(email);
+	public String home( Model model,@RequestParam(required=false,name="password") String pwd) {
 
+		  UserAccountDt udt = service.findByEmail(UtilityTool.getTokenEmail());
+		 
 		  UserAccountMt account = udt.getUserAccountMt();
 		  model.addAttribute("account", account);
-		  System.out.println(account.getUserId());
+		  
+		  System.out.println(pwd);
 
+		  if(pwd != null) {
+			  account.setPassword(bcryptoEncoder.encode(pwd));
+			  System.out.println("已嘗試");
+		  }
+		 
+		  
+	
 
 
 		return "main";
 	}
+	@GetMapping("/editpwd")
+	public String editpwd( Model model) {
+		
+		 UserAccountDt accountDetail = service.findByEmail(UtilityTool.getTokenEmail());
+			
+			UserAccountMt account = accountDetail.getUserAccountMt();
+			
+			model.addAttribute("Account", account);
+		
+			
+			
+			return "editpwd";
+
+	}
+	
+
 
 	@GetMapping("/editAccount")
 	public String editPage( Model model) {
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		  String email="";
-		  if (principal instanceof AccountUserDetails) { //帳號密碼登入
-			   email = ((AccountUserDetails)principal).getEmail();
-			  } else if(principal instanceof  DefaultOidcUser){ //GOOGLE登入
-			    email = ((DefaultOidcUser)principal).getEmail();
-			  }
-		 	
-	
-		UserAccountDt accountDetail = service.findByEmail(email);
+		
+		
+		 UserAccountDt accountDetail = service.findByEmail(UtilityTool.getTokenEmail());
+		
 		UserAccountMt account = accountDetail.getUserAccountMt();
 		
 		model.addAttribute("Account", account);
@@ -90,15 +98,8 @@ public class AccountController {
 	@GetMapping("/progessDeleteAccount")
 	public String processDelete( Model model) {
 		
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		  String email="";
-		  if (principal instanceof AccountUserDetails) { //帳號密碼登入
-			   email = ((AccountUserDetails)principal).getEmail();
-			  } else if(principal instanceof  DefaultOidcUser){ //GOOGLE登入
-			    email = ((DefaultOidcUser)principal).getEmail();
-			  }
-
-			UserAccountDt accountDetail = service.findByEmail(email);
+		UserAccountDt accountDetail = service.findByEmail(UtilityTool.getTokenEmail());
+		
 			UserAccountMt account = accountDetail.getUserAccountMt();
 
 		if (("admin").equals(account.getUserstatus().getAccountLevel().toString())) {
@@ -122,6 +123,23 @@ public class AccountController {
 		  accountdt.setGender(acd.getGender());
 		  accountdt.setBirth(acd.getBirth());
 		UserAccountMt account= accountdt.getUserAccountMt();
+		account.setUserAccountDt(accountdt);
+		service.edit(account);
+
+		model.addAttribute("text", "修改已完成");
+		return "index";
+	}
+	
+	@PostMapping("/progessEditPwd")
+	public String processEdit( Model model,@ModelAttribute("Account") UserAccountMt acc) {
+		
+		
+		  UserAccountDt accountdt = service.findByEmail(UtilityTool.getTokenEmail());
+		  UserAccountMt account= accountdt.getUserAccountMt();
+		  
+		  account.setPassword(bcryptoEncoder.encode(acc.getPassword()));
+		  
+		
 		account.setUserAccountDt(accountdt);
 		service.edit(account);
 
