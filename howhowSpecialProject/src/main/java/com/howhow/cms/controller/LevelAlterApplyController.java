def11804750e3764afdfdd0468668e2e08f6af52
ak99112 <ak99112@gmail.com>
2022-04-12 10:38:35 +0800

    little change

--- howhowSpecialProject/src/main/java/com/howhow/cms/controller/LevelAlterApplyController.java
+++ howhowSpecialProject/src/main/java/com/howhow/cms/controller/LevelAlterApplyController.java
@@ -1,6 +1,5 @@
 package com.howhow.cms.controller;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
