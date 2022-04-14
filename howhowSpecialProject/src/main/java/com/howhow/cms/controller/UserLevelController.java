8100760bd6b3f57923a3378286bbd74436e71fea
ak99112 <ak99112@gmail.com>
2022-04-14 15:10:24 +0800

    add apply api

--- howhowSpecialProject/src/main/java/com/howhow/cms/controller/UserLevelController.java
+++ howhowSpecialProject/src/main/java/com/howhow/cms/controller/UserLevelController.java
@@ -13,14 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.azure.core.annotation.Put;
+import com.howhow.account.service.UserAccountDtService;
 import com.howhow.account.service.UserStatusService;
+import com.howhow.cms.dto.LevelAlterApplyDTO;
 import com.howhow.cms.service.LevelAlterApplyService;
 import com.howhow.entity.LevelAlterApply;
 import com.howhow.entity.UserAccountDt;
 import com.howhow.entity.UserStatus;
+import com.howhow.util.UtilityTool;
 
 @Controller
-@RequestMapping("/cms")
 public class UserLevelController {
 	
 	@Autowired
@@ -29,27 +32,31 @@ public class UserLevelController {
 	@Autowired
 	private LevelAlterApplyService laas;
 	
-	@GetMapping("/userstatus")
+	@Autowired
+	private UserAccountDtService uads;
+	
+	@GetMapping("/cms/userstatus")
 	public String guidToUserStatus() {
 		return "cms/userstatusmain.html";
 	}
 	
 	// 搜尋指定用戶狀態
 	@ResponseBody
-	@GetMapping("/userstatusdata/{userid}")
+	@GetMapping("/cms/userstatusdata/{userid}")
 	public UserStatus findUserStatus(@PathVariable("userid") int userID) {
 		return uss.findById(userID);
 	}
 	
 	// 搜尋申請狀態變動用戶
 	@ResponseBody
-	@GetMapping("/applydata")
+	@GetMapping("/cms/applydata")
 	public List<LevelAlterApply> searchUser(){
 		return laas.findUnprocessApply("未處理");
 	}
 	
+	// 更新申請處理狀態
 	@ResponseBody
-	@PutMapping("/applydata")
+	@PutMapping("/cms/applydata")
 	public List<LevelAlterApply> updateApply(@RequestBody LevelAlterApply apply){
 		UserAccountDt userDtObj = laas.findById(apply.getApplyid()).getUserAccountDt();
 		apply.setUserAccountDt(userDtObj);
@@ -59,8 +66,22 @@ public class UserLevelController {
 	
 	// 更改指定用戶等級
 	@ResponseBody
-	@PutMapping("/userstatusdata")
+	@PutMapping("/cms/userstatusdata")
 	public UserStatus updateStatus(@RequestBody UserStatus userStatus) {
 		return uss.updateUserStatus(userStatus);
+	}
+	
+	// 申請成為老師
+	@PostMapping("api/applydata")
+	public boolean addApply(@RequestBody LevelAlterApplyDTO applyDTO) {
+		LevelAlterApply apply = new LevelAlterApply();
+		apply.setApplylevel("Teacher");
+		apply.setApplystatus("未處理");
+		apply.setSystemtime(UtilityTool.getSysTime());
+		apply.setUserAccountDt(uads.findById(applyDTO.getUserid()));
+		
+		laas.insertApply(apply);
+		
+		return true;
 	}
 }
---
+++ howhowSpecialProject/src/main/java/com/howhow/cms/dto/LevelAlterApplyDTO.java
@@ -0,0 +1,13 @@
+package com.howhow.cms.dto;
+
+public class LevelAlterApplyDTO {
+	private int userid;
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+}
